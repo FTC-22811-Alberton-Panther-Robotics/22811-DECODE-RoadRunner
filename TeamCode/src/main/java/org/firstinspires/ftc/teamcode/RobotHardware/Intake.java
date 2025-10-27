48f5e7f026dbbf34038eed7fb6e9050fa3a722c1
AvergeAxis <ashleyschatz87@gmial.com>
2025-10-27 17:39:08 -0600

    The intake was worked on for about 50 min

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware/Intake.java
@@ -1,6 +1,24 @@
 package org.firstinspires.ftc.teamcode.RobotHardware;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Intake {
- /// What am i suppose to do???? AAAAAAHHHHHHH?? 0_0
-    final double
+ /// i dont know what im doing!!!! AAAAAAHHHHHHH?? 0_0
+    private final double feedTime = .20;
+    /// EatSpeed is how fast it sucks the ball into the intake system
+    private final double eatPower = .10;
+    private DcMotor intake;
+
+    private enum IntakeState {ON, OFF}
+    private IntakeState intakeState;
+
+    public void init(HardwareMap hwMap) {
+        intake = hwMap.get(DcMotor.class, "intake");
+    }
+
+    public void eat() {
+        intake.setPower(eatPower);
+        intakeState = IntakeState.ON;
+    }
 }
